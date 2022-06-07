8bd7d0a1de99d328f7ea14306deafe3f3f9bee19
9ye1 <59861971+9ye1@users.noreply.github.com>
2022-06-07 22:43:30 +0900

    패이지 요청 처리 및 db 저장

--- oss_teamproject/boardsite/board/urls.py
+++ oss_teamproject/boardsite/board/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.main, name="mainpage"),  # 메인 페이지 연결
     path('bulletin', views.bulletin, name="bulletinboard"),  # 게시판 뷰 연결
     path('info', views.userinfo, name="userinfo"),   # 유저 정보 뷰 연결
+    path('register', views.register, name="register"), # 회원 가입 페이지로 연결
 ]
